478f708668169a0b27203cd9c72d2ebae17a279a
Florian Stockinger <florian.stockinger@it.htl-hl.ac.at>
2025-06-13 13:05:26 +0200

    final build

---
+++ backend/public/chunk-6RSIECBZ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-S7IWAG76.js";import"./chunk-6UB5JURL.js";export{a as HomeComponent};
---
+++ backend/public/chunk-D3INBW7O.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-EMT64UJO.js";import"./chunk-6M3OYAIA.js";import"./chunk-TQHVSWNO.js";import"./chunk-MH4W7LBN.js";import"./chunk-6UB5JURL.js";export{a as StockListComponent};
